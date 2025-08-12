d1d9953aada171e22906fdbf7585f9b99f77969f
Matthew <mattwoods301@gmail.com>
2025-08-11 23:43:07 -0400

    changed files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: 'https://github.com/Mwoods30/React-To-Do-List',
-    plugins: [react()],
+  plugins: [react()],
 })
